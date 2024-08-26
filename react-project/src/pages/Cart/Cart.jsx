ddd7dae939c80660ec56701cfab765a589507817
Hasandee <hasandisethara@gmail.com>
2024-08-26 15:13:24 +0530

    Create cart page
    Create cart page

--- react-project/src/pages/Cart/Cart.jsx
+++ react-project/src/pages/Cart/Cart.jsx
@@ -1,13 +1,80 @@
-import React from 'react';
-import './Cart.css'
-
+import React, { useContext } from 'react';
+import './Cart.css';
+import { StoreContext } from '../../context/StoreContext';
 
 const Cart = () => {
-  return (
-    <div>
-       <h1 className="mt-5" >Cart</h1>
-    </div>
-  )
-}
+  const { cartItems, food_list, removeFromCart, getTotalCartAmount } = useContext(StoreContext); // Corrected the function name
 
-export default Cart
+  return (
+    <div className='cart'>
+      <div className='cart-items'>
+        <div className='cart-items-title'>
+          <p>Items</p>
+          <p>Title</p>
+          <p>Price</p>
+          <p>Quantity</p>
+          <p>Total</p>
+          <p>Remove</p>
+        </div>
+        <br />
+        <hr />
+        {food_list.map((item, index) => {
+          if (cartItems[item._id] > 0) {
+            return (
+              <div key={index}> {/* Moved the key to the correct div */}
+                <div className='cart-items-title cart-items-item'>
+                  <img src={item.image} alt="" />
+                  <p>{item.name}</p>
+                  <p>${item.price}</p>
+                  <p>{cartItems[item._id]}</p>
+                  <p>${item.price * cartItems[item._id]}</p>
+                  <p 
+                    className="remove-button" 
+                    onClick={() => removeFromCart(item._id)}>
+                    x
+                  </p>
+                </div>
+                <hr />
+              </div>
+            );
+          }
+          return null; // Added return null to avoid undefined return
+        })}
+      </div>
+
+      <div className="cart-bottom">
+        <div className="cart-total">
+          <h2>Cart Total</h2>
+          <div>
+            <div className="cart-total-details">
+              <p>SubTotal</p>
+              <p>${getTotalCartAmount()}</p> {/* Corrected the function name here */}
+            </div>
+            <hr />
+            <div className="cart-total-details">
+              <p>Delivery Fee</p>
+              <p>$2</p> {/* Added dollar sign for consistency */}
+            </div>
+            <hr />
+            <div className="cart-total-details">
+              <b>Total</b>
+              <b>${getTotalCartAmount() + 2}</b> {/* Corrected the function name here */}
+            </div>
+          </div>
+          <button>Proceed to Checkout</button>
+        </div>
+        <div className="cart-promocode">
+          <div>
+            <p>If you have a promo code, enter it here</p>
+            <div className='cart-promocode-input'>
+              <input type="text" placeholder='promo code' />
+              <button>Submit</button>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Cart;
