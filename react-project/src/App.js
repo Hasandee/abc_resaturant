f9cf5309f36f51e8623184a4e6c03a4efd5a0485
Hasandee <hasandisethara@gmail.com>
2024-08-26 09:35:23 +0530

    Create Login pupup
    Create Login page as popup

--- react-project/src/App.js
+++ react-project/src/App.js
@@ -1,3 +1,4 @@
+import React, { useState } from 'react';
 import './App.css';
 import { Link, Routes, Route } from 'react-router-dom';
 import Container from 'react-bootstrap/Container';
@@ -11,58 +12,68 @@ import Reservation from './pages/Reservation';
 import About from './pages/About';
 import Contact from './pages/Contact';
 import { FaShoppingCart } from 'react-icons/fa'; 
-import React from 'react';
 import Cart from './pages/Cart/Cart';
 import Admin from './components/Admin';
 import PlaceOrder from './pages/PlaceOrder/PlaceOrder';
 import Footer from './components/Footer/Footer';
+import LoginPopup from './components/LoginPopup/LoginPopup'; // Assuming you have a LoginPopup component
 
-function App() {
+const App = () => {
+  const [showLogin, setShowLogin] = useState(false);
+
   return (
-    <div>
-      <Navbar expand="lg" className='fixed-top bg-body-tertiary shadow'>
-        <Container>
-          <Navbar.Brand>
-            <Link to="/" className='navbar-brand text-success fw-semibold'>
-              ABC Restaurant
-            </Link>
-          </Navbar.Brand>
-          <Navbar.Toggle aria-controls='basic-navbar-nav' />
-          <Navbar.Collapse id='basic-navbar-nav'>
-            <Nav className='me-auto justify-content-end w-100'>
-              <Link to="/" className='nav-link active text-uppercase'>Home</Link>
-              <Link to="/menu" className='nav-link text-uppercase'>Menu</Link>
-              <Link to="/gallery" className='nav-link text-uppercase'>Gallery</Link>
-              <Link to="/reservation" className='nav-link text-uppercase'>Reservation</Link>
-              <Link to="/about" className='nav-link text-uppercase'>About</Link>
-              <Link to="/contact" className='nav-link text-uppercase'>Contact</Link>
-            </Nav>
-            <Nav>
-              <Button variant='outline-success' className='text-uppercase'>
-                Sign In
-              </Button>
-              <Link to='/cart' className='nav-link text-uppercase cart-icon' aria-label="Cart">
-                <FaShoppingCart style={{ fontSize: '2rem' }} />
+    <>
+      {showLogin ? <LoginPopup setShowLogin={setShowLogin}/>:<></>}
+
+      <div className='app'>
+        <Navbar expand="lg" className='fixed-top bg-body-tertiary shadow'>
+          <Container>
+            <Navbar.Brand>
+              <Link to="/" className='navbar-brand text-success fw-semibold'>
+                ABC Restaurant
               </Link>
-            </Nav>
-          </Navbar.Collapse>
-        </Container>
-      </Navbar>
+            </Navbar.Brand>
+            <Navbar.Toggle aria-controls='basic-navbar-nav' />
+            <Navbar.Collapse id='basic-navbar-nav'>
+              <Nav className='me-auto justify-content-end w-100'>
+                <Link to="/" className='nav-link active text-uppercase'>Home</Link>
+                <Link to="/menu" className='nav-link text-uppercase'>Menu</Link>
+                <Link to="/gallery" className='nav-link text-uppercase'>Gallery</Link>
+                <Link to="/reservation" className='nav-link text-uppercase'>Reservation</Link>
+                <Link to="/about" className='nav-link text-uppercase'>About</Link>
+                <Link to="/contact" className='nav-link text-uppercase'>Contact</Link>
+              </Nav>
+              <Nav>
+                <Button 
+                  variant='outline-success' 
+                  className='text-uppercase' 
+                  onClick={() => setShowLogin(true)} // Trigger the login popup
+                >
+                  Sign In
+                </Button>
+                <Link to='/cart' className='nav-link text-uppercase cart-icon' aria-label="Cart">
+                  <FaShoppingCart style={{ fontSize: '2rem' }} />
+                </Link>
+              </Nav>
+            </Navbar.Collapse>
+          </Container>
+        </Navbar>
 
-      <Routes>
-        <Route path='/' element={<Home />} />
-        <Route path='/menu' element={<Menu />} /> 
-        <Route path='/gallery' element={<Gallery />} />
-        <Route path='/reservation' element={<Reservation />} />
-        <Route path='/about' element={<About />} />
-        <Route path='/contact' element={<Contact />} />
-        <Route path="/admin" element={<Admin />} />
-        <Route path="/cart" element={<Cart />} />
-        <Route path="/order" element={<PlaceOrder />} />
-      </Routes>
+        <Routes>
+          <Route path='/' element={<Home />} />
+          <Route path='/menu' element={<Menu />} /> 
+          <Route path='/gallery' element={<Gallery />} />
+          <Route path='/reservation' element={<Reservation />} />
+          <Route path='/about' element={<About />} />
+          <Route path='/contact' element={<Contact />} />
+          <Route path="/admin" element={<Admin />} />
+          <Route path="/cart" element={<Cart />} />
+          <Route path="/order" element={<PlaceOrder />} />
+        </Routes>
 
-    <Footer />
-    </div>
+        <Footer />
+      </div>
+    </>
   );
 }
 
